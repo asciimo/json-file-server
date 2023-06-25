ef186eb8d81427441ff4aabe2d56e3dfe3e995a6
Steve Calnan <steve.calnan@datadoghq.com>
2023-06-24 22:37:54 -0700

    working json file server with collection and item endpoints and configurable routes.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const fastify = require("fastify")({ logger: true });
 const path = require("path");
+const fs = require("node:fs");
+
+const staticPath = path.join(__dirname, "public");
 
 // read a configuration object from the file config.json
-// const config =
-// @todo use the configuration value for routes `/${COLLECTION_ROUTE}
-// and `/${ITEM_ROUTE}`
+// and parse
+const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 
-const COLLECTION_ROUTE = "files";
-const ITEM_ROUTE = "file";
+const COLLECTION_ROUTE = config.collection || "files";
+const ITEM_ROUTE = config.item || "file";
+
+fastify.log.warn(`Collection route: ${COLLECTION_ROUTE}`);
+fastify.log.warn(`Item route: ${ITEM_ROUTE}`);
 
 fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
+  root: staticPath,
   prefix: "/public/",
 });
 
@@ -21,11 +26,20 @@ fastify.get("/", (req, reply) => {
 });
 
 fastify.get(`/${COLLECTION_ROUTE}`, (req, reply) => {
-  reply.code(200).sendFile("hello.json");
+  const files = fs
+    .readdirSync(staticPath)
+    .filter((file) => file.endsWith(".json"));
+  fastify.log.info(files);
+  reply.code(200).send(files);
 });
 
-fastify.get(`/${ITEM_ROUTE}:id`, (req, reply) => {
-  reply.code(200).sendFile("hello.json");
+fastify.get(`/${ITEM_ROUTE}/:id`, (req, reply) => {
+  const file = `${req.params.id}.json`;
+  if (fs.existsSync(`${staticPath}/${file}`)) {
+    reply.code(200).sendFile(file);
+  } else {
+    reply.code(404).send({ error: "File not found" });
+  }
 });
 
 // Run the server!
