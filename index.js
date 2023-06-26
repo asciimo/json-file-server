6a84f7c43bab58a01c1d16b5a4b62297cb2090bb
Steve Calnan <asciimo@gmail.com>
2023-06-25 23:00:32 -0700

    collection endpoint returns all json contents in an array

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const fs = require("node:fs");
 
 const staticPath = path.join(__dirname, "public");
 
-// read a configuration object from the file config.json
-// and parse
 const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
-
 const COLLECTION_ROUTE = config.collection || "files";
 const ITEM_ROUTE = config.item || "file";
 const PORT = config.port || 3000;
-
-fastify.log.warn(`Collection route: ${COLLECTION_ROUTE}`);
-fastify.log.warn(`Item route: ${ITEM_ROUTE}`);
 
 fastify.register(require("@fastify/static"), {
   root: staticPath,
@@ -23,17 +17,25 @@ fastify.register(require("@fastify/static"), {
 fastify.register(require("@fastify/cors"), { cors: "*" });
 
 fastify.get("/", (req, reply) => {
-  reply.code(200).sendFile("hello.json");
+  reply.code(200).send({ message: "Welcome to the JSON file server API!" });
 });
 
+// Return all files concatenated into a single JSON array
 fastify.get(`/${COLLECTION_ROUTE}`, (req, reply) => {
+  const contents = [];
   const files = fs
     .readdirSync(staticPath)
     .filter((file) => file.endsWith(".json"));
-  fastify.log.info(files);
-  reply.code(200).send(files);
+  files.forEach((file) => {
+    const content = JSON.parse(
+      fs.readFileSync(`${staticPath}/${file}`, "utf8")
+    );
+    contents.push(content);
+  });
+  reply.code(200).send(contents);
 });
 
+// Return a single JSON file
 fastify.get(`/${ITEM_ROUTE}/:id`, (req, reply) => {
   const file = `${req.params.id}.json`;
   if (fs.existsSync(`${staticPath}/${file}`)) {
